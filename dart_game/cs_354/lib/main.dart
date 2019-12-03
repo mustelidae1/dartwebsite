b61e57f664c9d599d7ffaa26f17326fd5666aaad
mustelidae1 <othm@live.com>
2019-12-03 08:56:20 -0700

    comments

--- dart_game/cs_354/lib/main.dart
+++ dart_game/cs_354/lib/main.dart
@@ -10,15 +10,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Tic-Tac-Toe',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: MyHomePage(title: 'Tic-Tac-Toe'),
@@ -28,16 +19,6 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -134,7 +115,7 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   bool isWin(int i, int p) {
-//Initializing neede variables
+//Initializing needed variables
     int rangeMin =0;
     int rangeMax =0;
     String winningText = (playerText(p));
@@ -271,16 +252,9 @@ if (i == 8){
   @override
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    // Run every time setState is called 
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: 
