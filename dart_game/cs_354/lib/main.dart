20e6a8507987b33a4785a93dfa5a0c6fc66f403d
mustelidae1 <othm@live.com>
2019-12-03 08:57:41 -0700

    comments

--- dart_game/cs_354/lib/main.dart
+++ dart_game/cs_354/lib/main.dart
@@ -4,7 +4,6 @@ import 'package:cs_354/tic_tac_toe_button.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
